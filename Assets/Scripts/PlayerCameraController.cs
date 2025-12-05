741e0067a82df8a7593e55543a1674c8fbd9519f
nemehecat123 <matevz4ever@gmail.com>
2025-12-05 17:39:00 +0100

    forgot in the previous commit

---
+++ Assets/Scripts/PlayerCameraController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using Unity.Netcode;
+using Unity.Cinemachine;
+
+public class PlayerCameraController : NetworkBehaviour
+{
+    private CinemachineCamera virtualCam;
+
+    void Awake()
+    {
+        virtualCam = GetComponentInChildren<CinemachineCamera>();
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        // Only enable the camera if this is MY player
+        if (!IsOwner)
+        {
+            virtualCam.enabled = false;
+        }
+        else
+        {
+            virtualCam.enabled = true;
+        }
+    }
+}
