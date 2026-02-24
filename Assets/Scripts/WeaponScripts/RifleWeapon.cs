8a2c55efafbef05cceb59272b190d69d43dc1792
nemehecat123 <matevz4ever@gmail.com>
2026-02-24 12:30:26 +0100

    added the same scopes to the rifle so it moves a bit slower in the zoom and the fov is difrent. Now make sure the walking speed is slower also

--- Assets/Scripts/WeaponScripts/RifleWeapon.cs
+++ Assets/Scripts/WeaponScripts/RifleWeapon.cs
@@ -4,13 +4,111 @@ using UnityEngine;
 using UnityEngine.UIElements;
 
 public class RifleWeapon : WeaponStats
-
 {
+
+    [SerializeField] private float zoomFOV = 5f;
+
+    [SerializeField] private float inZoomMouseSpeed;
+
 
     protected override void Aim() // pow poveci malo
 
     {
-        throw new System.NotImplementedException();
+        if (!IsOwner) return;
+        Debug.Log(" is zoomed variable :" + isZoomed);
+
+        if (!isZoomed && !isReloading) // zoomej ce nisi zoomed
+        {
+            EnableZoom();
+        }
+        else
+        {
+            DisableZoom();
+        }
+    }
+
+    public void EnableZoom()
+    {
+        isZoomed = true;
+
+        virtualCamera.Lens.FieldOfView = zoomFOV;
+
+        //CinemachinePOV.m_HorizontalAxis.m_MaxSpeed =3;
+
+
+
+        setCameraSpeedtoSlow();
+
+        Debug.Log("zoom is " + virtualCamera.Lens.FieldOfView);
+
+
+
+        //    virtualCamera.Lens.FieldOfView = Mathf.Lerp(
+        //    virtualCamera.Lens.FieldOfView,
+        //    zoomFOV,
+        //    Time.deltaTime * zoomSpeed
+        //);
+    }
+
+
+
+
+    public override void OnUnequip()
+    {
+        if (isZoomed)
+            DisableZoom();
+    }
+
+    public void DisableZoom()
+    {
+        if (!IsOwner) return;
+        virtualCamera.Lens.FieldOfView = defaultFOV;
+        //scopeOverlayUI.SetActive(false);
+        isZoomed = false;
+        setCameraSpeedtoNormal();
+        Debug.Log("zoom is " + virtualCamera.Lens.FieldOfView);
+
+    }
+
+    public void setCameraSpeedtoSlow()
+    {
+        foreach (var c in inputAxisController.Controllers)
+        {
+
+
+            if (c.Name == "Look X (Pan)")
+            {
+
+                c.Input.Gain = inZoomMouseSpeed;
+            }
+
+            if (c.Name == "Look Y (Tilt)")
+            {
+                c.Input.Gain = -inZoomMouseSpeed;
+
+            }
+
+        }
+    }
+    public void setCameraSpeedtoNormal()
+    {
+        foreach (var c in inputAxisController.Controllers)
+        {
+
+
+            if (c.Name == "Look X (Pan)")
+            {
+
+                c.Input.Gain = 1; //constant... to je konstanta za normal playtrough
+            }
+
+            if (c.Name == "Look Y (Tilt)")
+            {
+                c.Input.Gain = -1; //constant... to je konstanta za normal playtrough
+
+            }
+
+        }
     }
 
 
@@ -22,7 +120,7 @@ public class RifleWeapon : WeaponStats
 
         Vector3 endPoint = origin + baseDir * maxDistance;
 
-       
+
         if (Physics.Raycast(origin, baseDir, out RaycastHit hit, maxDistance, layerMask))
         {
             endPoint = hit.point;
@@ -40,5 +138,4 @@ public class RifleWeapon : WeaponStats
         SpawnTracerClientRpc(origin, endPoint);
     }
 
-   
 }
