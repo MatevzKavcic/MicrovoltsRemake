1cd1f202eeaf2acb16d3956cad28f99eeca6ed96
nemehecat123 <matevz4ever@gmail.com>
2026-05-13 12:22:03 +0200

    FIXED THE BODY ROTATION.... the camera was not assigning correclty.... networking is fun fun

--- Assets/Scripts/BodyRotation.cs
+++ Assets/Scripts/BodyRotation.cs
@@ -1,25 +1,48 @@
+using System.Collections;
 using Unity.Netcode;
 using UnityEngine;
 
 public class BodyRotation : NetworkBehaviour
 {
     [SerializeField] private Transform spineBone;
+
     [SerializeField] private float maxUpAngle = 40f;
     [SerializeField] private float maxDownAngle = -30f;
     [SerializeField] private float spineRotationSpeed = 10f;
 
-    private float currentSpineAngle;
     private Quaternion spineInitialRotation;
 
-
     private Camera playerCamera;
+
+    private float currentSpineAngle;
+
+    private NetworkVariable<float> networkSpineAngle =
+        new NetworkVariable<float>(
+            0f,
+            NetworkVariableReadPermission.Everyone,
+            NetworkVariableWritePermission.Owner
+        );
 
     public override void OnNetworkSpawn()
     {
         if (!IsOwner) return;
 
-        playerCamera = Camera.main;
+        StartCoroutine(WaitForCamera());
     }
+
+
+    private IEnumerator WaitForCamera()
+    {
+        while (playerCamera == null)
+        {
+            playerCamera = Camera.main;
+
+            yield return null;
+        }
+
+        Debug.Log("Camera acquired for client");
+    }
+
 
     void Start()
     {
@@ -28,21 +51,27 @@ public class BodyRotation : NetworkBehaviour
 
     void LateUpdate()
     {
-        if (!IsOwner) return;
 
-        if (playerCamera == null) return;
+        Debug.Log($"IsOwner={IsOwner} | CameraNull={playerCamera == null}");
+        if (IsOwner && playerCamera != null)
+        {
 
+            Debug.Log($"IsOwner: {IsOwner} | Local: {NetworkManager.Singleton.LocalClientId} | Owner: {OwnerClientId}");
 
-        float cameraPitch = playerCamera.transform.eulerAngles.x;
+            float cameraPitch = playerCamera.transform.eulerAngles.x;
 
-        if (cameraPitch > 180f)
-            cameraPitch -= 360f;
+            if (cameraPitch > 180f)
+                cameraPitch -= 360f;
 
-        float targetAngle = Mathf.Clamp(cameraPitch, maxDownAngle, maxUpAngle);
+            float targetAngle =
+                Mathf.Clamp(cameraPitch, maxDownAngle, maxUpAngle);
+
+            networkSpineAngle.Value = targetAngle;
+        }
 
         currentSpineAngle = Mathf.Lerp(
             currentSpineAngle,
-            targetAngle,
+            networkSpineAngle.Value,
             spineRotationSpeed * Time.deltaTime
         );
 
